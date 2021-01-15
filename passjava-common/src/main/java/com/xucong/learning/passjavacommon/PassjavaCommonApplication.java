a8e1775e624b70d37fe9215dd825fa0002992ffb
xucong_i <>
2021-01-15 18:26:07 +0800

    创建common

---
+++ passjava-common/src/main/java/com/xucong/learning/passjavacommon/PassjavaCommonApplication.java
@@ -0,0 +1,13 @@
+package com.xucong.learning.passjavacommon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassjavaCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PassjavaCommonApplication.class, args);
+	}
+
+}
---
+++ passjava-common/src/test/java/com/xucong/learning/passjavacommon/PassjavaCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xucong.learning.passjavacommon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PassjavaCommonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
